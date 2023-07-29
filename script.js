8271fcd92853023edab1abb8218d49b404cf11e2
Андрей Мазунин (Andrey Mazunin) <97140182+js-easy-school@users.noreply.github.com>
2023-07-29 11:03:57 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
 window.addEventListener('DOMContentLoaded', function () {
-    var battery = navigator.battery || navigator.webkitBattery || navigator.mozBattery;
-
     function updateBatteryStatus() {
-        var timeRemaining = battery.dischargingTime;
-        if (isNaN(timeRemaining) || timeRemaining === Infinity) {
-            document.getElementById('timeRemaining').textContent = 'Неизвестно';
-        } else {
-            var hours = Math.floor(timeRemaining / 60);
-            var minutes = timeRemaining % 60;
-            document.getElementById('timeRemaining').textContent = hours + ' часов ' + minutes + ' минут';
-        }
+        navigator.getBattery().then(function (battery) {
+            var timeRemaining = battery.dischargingTime;
+            if (isNaN(timeRemaining) || timeRemaining === Infinity) {
+                document.getElementById('timeRemaining').textContent = 'Неизвестно';
+            } else {
+                var hours = Math.floor(timeRemaining / 60);
+                var minutes = timeRemaining % 60;
+                document.getElementById('timeRemaining').textContent = hours + ' часов ' + minutes + ' минут';
+            }
+        });
     }
 
     updateBatteryStatus();
 
-    battery.addEventListener('chargingchange', updateBatteryStatus);
-    battery.addEventListener('levelchange', updateBatteryStatus);
+    navigator.getBattery().then(function (battery) {
+        battery.addEventListener('chargingchange', updateBatteryStatus);
+        battery.addEventListener('levelchange', updateBatteryStatus);
+    });
 });
